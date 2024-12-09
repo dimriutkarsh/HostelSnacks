56f330ef33cd2f3f02052bed8c8734769868581c
UtkarshDimri <dimriutkarsh55@gmail.com>
2024-12-10 00:55:41 +0530

    reduced the no of items in the menu

--- assets/js/data.js
+++ assets/js/data.js
@@ -7,19 +7,19 @@ const menuItems = [
         image: "./assets/images/maggi.jpg"
 
     },
-    {
-        id: 2,
-        name: 'Lays Classic',
-        price: 20,
-        // image: 'https://images.unsplash.com/photo-1566478989037-eec170784d0b?auto=format&fit=crop&w=800&q=80'
-        image: "./assets/images/lays.jpg"
-    },
+    // {
+    //     id: 2,
+    //     name: 'Lays Classic',
+    //     price: 20,
+    //     // image: 'https://images.unsplash.com/photo-1566478989037-eec170784d0b?auto=format&fit=crop&w=800&q=80'
+    //     image: "./assets/images/lays.jpg"
+    // },
     {
         id: 3,
-        name: 'Kurkure',
+        name: 'Takatak Chips',
         price: 20,
         // image: 'https://images.unsplash.com/photo-1613919113640-25732ec5e61f?auto=format&fit=crop&w=800&q=80'
-        image: "./assets/images/maggi.jpg"
+        image: "./assets/images/takatak chips.jpg"
     },
     {
         id: 4,
